2c18fe6ab6df8f7dbea35043e430b8bca3379321
Filipe Medeiros <filipesilvamedeiros@gmail.com>
2021-09-25 17:11:05 +0100

    first commit

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,20 @@
+<div class="outter">
+  <div class="inner"><slot /></div>
+</div>
+
+<style lang="scss">
+  .outter {
+    display: flex;
+    justify-content: center;
+    width: 100%;
+
+    .inner {
+      $breakpoints: 320px, 540px, 768px, 1280px;
+      @each $breakpoint in $breakpoints {
+        @media screen and (min-width: $breakpoint) {
+          max-width: $breakpoint;
+        }
+      }
+    }
+  }
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,26 @@
+<script lang="ts"></script>
+
+<h1>
+  <span class="politician">Político</span><br /><span class="or">ou</span><br /><span
+    class="real-estate-agent">Agente Imobiliário</span
+  >
+</h1>
+
+<style lang="scss">
+  h1 {
+    text-align: center;
+    line-height: 1;
+
+    .or {
+      font-size: smaller;
+    }
+
+    .politician {
+      color: blue;
+    }
+
+    .real-estate-agent {
+      color: rgb(149, 18, 18);
+    }
+  }
+</style>
